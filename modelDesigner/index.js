c4464876087d95ccc370637645145f0d0da86a47
Gowrav Vishwakarma <gowravvishwakarma@gmail.com>
2021-01-26 17:38:12 +0530

    WIP modelDesigner and VNATK Crud updated for Array based data

--- modelDesigner/index.js
+++ modelDesigner/index.js
@@ -5,12 +5,24 @@ module.exports = {
 
     init: async function (req, res, next, Models) {
         var models = {};
+
         Object.keys(Models).forEach(modelName => {
             if (modelName === 'sequelize' || modelName === 'Sequelize') return;
-            models[modelName] = { rawAttributes: Models[modelName].rawAttributes, associations: VNATKClientHelpers.getAssociations(Models[modelName]) };
+
+            // Object.keys(Models[modelName]).forEach(prop => {
+            //     console.log(prop, Models[modelName][prop]);
+            // });
+            var fields = {};
+            Object.keys(Models[modelName].rawAttributes).forEach(field => {
+                fields[field] = Models[modelName].rawAttributes[field];
+                fields[field]['type']['key'] = Models[modelName].rawAttributes[field].type.key;
+            });
+
+            models[modelName] = { name: modelName, tableName: Models[modelName].tableName, rawAttributes: fields, associations: VNATKClientHelpers.getAssociations(Models[modelName]) };
 
         })
-        res.send(models);
+        const data = { models, sequlize: {} };
+        res.send(data);
     },
 
     save: function (req, res, next, Models) {
